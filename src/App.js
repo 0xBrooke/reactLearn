8d7976dc10ee156bcdb47b39d494d50f8d6cf6b8
xiawenke@labifenqi.com <xiawenke@rongjiexin.com>
2019-08-19 19:25:56 +0800

    动画

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+// 动画组件
+import { CSSTransition } from 'react-transition-group';
+import './style.css'
 // 自带的
 // function App() {
 //   return (
@@ -7,20 +10,38 @@ import React from 'react';
 //         <p>
 //           welcome to 
 //         </p>
-     
+
 //       </header>
 //     </div>
 //   );
 // }
 
 //my render()中的内容部分也是jsx的语法 所以要首先引入react
-class App extends React.Component{
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      show: true
+    }
+    this.handleTaggole = this.handleTaggole.bind(this)
+  }
   render() {
     return (
-      <div>
-        我发顺丰似睡非睡
-      </div>
+      <Fragment>
+        <CSSTransition in={this.state.show} timeout={1000}>
+          <div>我发顺丰似睡非睡</div>
+        </CSSTransition>
+
+        <button onClick={this.handleTaggole}>taggole</button>
+      </Fragment>
+
     )
+  }
+
+  handleTaggole() {
+    this.setState({
+      show: this.state.show ? false : true
+    })
   }
 }
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import App from './App';
-import TodoList from './TodoList';
+import App from './App';
+// import TodoList from './TodoList';
 // 使用缓存 防止断网后，无法打开浏览的网页
 import * as serviceWorker from './serviceWorker';
 // jsx语法 可使用自己定义的组件
 // 注意：组件的开头必须大写字母
-ReactDOM.render(<TodoList/>, document.getElementById('root'));
+ReactDOM.render(<App/>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
