bf5f642badf7c120b02ecd210e5c9d5e744821c1
wkxia <13520207133@163.com>
2019-11-25 01:01:59 +0800

    redux

--- src/MyRedux.js
+++ src/MyRedux.js
@@ -1,15 +1,36 @@
 import React, { Component } from 'react';
 import 'antd/dist/antd.css';
-import { Input } from 'antd';
+import { Input, Button, List } from 'antd';
+import store from './store'
 class MyRedux extends Component {
-    render() {
-        return (
-            <div>
-                <Input type="text" placeholder='dd'/>
-            </div>
+  constructor(props) {
+    super(props);
+    this.state = store.getState()
+    console.log(store.getState())
+  }
+  render() {
+    return (
+      <div style={{ margin: '10px' }}>
+        <div>
+          <Input type="text" placeholder='dd' value={this.state.inputValue} style={{ width: '250px', marginRight: '10px' }} />
+          <Button type='primary'>提交</Button>
+        </div>
+        <div>
+          <List style={{ marginTop: '10px' }}
+            bordered
+            dataSource={this.state.list}
+            renderItem={item => (
+              <List.Item>
+                {item}
+              </List.Item>
+            )}
+          />
+        </div>
+      </div>
 
-        )
-    }
+
+    )
+  }
 }
 
 
---
+++ src/store/reducer.js
@@ -0,0 +1,7 @@
+const defaultStort = {
+  inputValue: '新方法',
+  list: [1,2]
+}
+export default (state = defaultStort, action) => {
+  return state;
+}
