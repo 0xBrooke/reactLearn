cd6703496616ee9a1d22adfc8c88c723dbf2ce71
wkxia <13520207133@163.com>
2019-11-30 00:27:37 +0800

    redux

--- src/MyRedux.js
+++ src/MyRedux.js
@@ -2,34 +2,64 @@ import React, { Component } from 'react';
 import 'antd/dist/antd.css';
 import { Input, Button, List } from 'antd';
 import store from './store'
+import {CHANGE_INPUT_VALUE,ADD_TODO_ITEM,DEL_TODO_ITEM} from './store/actionTypes'
 class MyRedux extends Component {
   constructor(props) {
     super(props);
-    this.state = store.getState()
-    console.log(store.getState())
+    this.state = store.getState();
+    this.changeInput = this.changeInput.bind(this);
+    this.handleStoreChange = this.handleStoreChange.bind(this);
+    this.handleBtnClick = this.handleBtnClick.bind(this);
+    store.subscribe(this.handleStoreChange)
   }
   render() {
     return (
       <div style={{ margin: '10px' }}>
         <div>
-          <Input type="text" placeholder='dd' value={this.state.inputValue} style={{ width: '250px', marginRight: '10px' }} />
-          <Button type='primary'>提交</Button>
+          <Input type="text"
+            placeholder='dd'
+            value={this.state.inputValue}
+            style={{ width: '250px', marginRight: '10px' }}
+            onChange={this.changeInput}
+          />
+          <Button type='primary' onClick={this.handleBtnClick}>提交</Button>
         </div>
         <div>
           <List style={{ marginTop: '10px' }}
             bordered
             dataSource={this.state.list}
-            renderItem={item => (
-              <List.Item>
+            renderItem={(item,index) => (
+              <List.Item onClick={this.handleBtnDel.bind(this,index)}>
                 {item}
               </List.Item>
             )}
           />
         </div>
       </div>
-
-
     )
+  }
+  changeInput(e) {
+    const action = {
+      type: CHANGE_INPUT_VALUE,
+      value: e.target.value,
+    }
+    store.dispatch(action);
+  }
+  handleStoreChange() {
+    this.setState(store.getState())
+  }
+  handleBtnClick() {
+    const action = {
+      type: ADD_TODO_ITEM,
+    };
+    store.dispatch(action)
+  }
+  handleBtnDel(index) {
+    const action = {
+      type: DEL_TODO_ITEM,
+      index:index
+    };
+    store.dispatch(action)
   }
 }
 
--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,7 +1,26 @@
+import {CHANGE_INPUT_VALUE,ADD_TODO_ITEM,DEL_TODO_ITEM} from './actionTypes'
 const defaultStort = {
-  inputValue: '新方法',
-  list: [1,2]
+  inputValue: '',
+  list: []
 }
+// reducer 可以接受state 但是绝对不能修改state
 export default (state = defaultStort, action) => {
+  console.log(state, action)
+  if (action.type === CHANGE_INPUT_VALUE) {
+    const newState = JSON.parse(JSON.stringify(state))
+    newState.inputValue = action.value;
+    return newState
+  }
+  if (action.type === ADD_TODO_ITEM) {
+    const newState = JSON.parse(JSON.stringify(state))
+    newState.list.push(newState.inputValue)
+    newState.inputValue = ''
+    return newState;
+  }
+  if (action.type === DEL_TODO_ITEM) {
+    const newState = JSON.parse(JSON.stringify(state))
+    newState.list.splice(action.index, 1);
+    return newState;
+  }
   return state;
 }
